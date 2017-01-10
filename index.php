da3e13e6ba88711eea9671f6942f385c9f6573f5
xiezhuohan <jasonxzh818@gmail.com>
2017-01-09 22:56:22 -0800

    fix broken url

--- index.php
+++ index.php
@@ -1,28 +1,34 @@
 <?php
 
 
-$domain = "http://104.198.0.197:8080";
+$domain = "https://congress.api.sunlightfoundation.com";
 $apikey = "307a2cb5894d4ba7a98f1f03672d00d3";
 
+$arrContextOptions = array(
+    "ssl"=>array(
+        "verify_peer"=>false,
+        "verify_peer_name"=>false,
+    ),
+);  
 
 
 //get legislators details
 if ($_GET['bioguide_id'] ) {
-    $data = file_get_contents($domain . "/legislators?per_page=all&bioguide_id=" . $_GET['bioguide_id'] . "&apikey=" . $apikey);
+    $data = file_get_contents($domain . "/legislators?per_page=all&bioguide_id=" . $_GET['bioguide_id'] . "&apikey=" . $apikey, false, stream_context_create($arrContextOptions));
     header('Content-type: application/json');
     echo  $data;
 }
 
 //get committees info from one legislators
 if ($_GET['committees'] ) {
-    $data = file_get_contents($domain . "/committees?member_ids=" . $_GET['committees'] . "&per_page=5&" . "&apikey=" . $apikey);
+    $data = file_get_contents($domain . "/committees?member_ids=" . $_GET['committees'] . "&per_page=5&" . "&apikey=" . $apikey, false, stream_context_create($arrContextOptions));
     header('Content-type: application/json');
     echo  $data;
 }
 
 //get bills info from one legislators
 if ($_GET['bills'] ) {
-    $data = file_get_contents($domain . "/bills?sponsor_id=" . $_GET['bills'] . "&per_page=5&" .  "&apikey=" . $apikey);
+    $data = file_get_contents($domain . "/bills?sponsor_id=" . $_GET['bills'] . "&per_page=5&" .  "&apikey=" . $apikey, false, stream_context_create($arrContextOptions));
     header('Content-type: application/json');
     echo $data;
 }
@@ -30,54 +36,54 @@ if ($_GET['bills'] ) {
 
 //get all house legislators
 if ( $_GET['house'] ) {
-    $data = file_get_contents($domain . "/legislators?per_page=all&chamber=house&apikey=" . $apikey);
+    $data = file_get_contents($domain . "/legislators?per_page=all&chamber=house&apikey=" . $apikey, false, stream_context_create($arrContextOptions));
     header('Content-type: application/json');
     echo $data;
 }
 
 //get all house legislators
 if ( $_GET['senate'] ) {
-    $data = file_get_contents($domain . "/legislators?per_page=all&chamber=senate&apikey=" . $apikey);
+    $data = file_get_contents($domain . "/legislators?per_page=all&chamber=senate&apikey=" . $apikey, false, stream_context_create($arrContextOptions));
     header('Content-type: application/json');
     echo $data;
 }
 
 //get all legislators
 if ( $_GET['legislators'] ) {
-    $data = file_get_contents($domain . "/legislators?per_page=all&apikey=" . $apikey);
+    $data = file_get_contents($domain . "/legislators?per_page=all&apikey=" . $apikey, false, stream_context_create($arrContextOptions));
     header('Content-type: application/json');
     echo  $data;
 }
 
 //get all bills
 if ( $_GET['allbills'] ) {
-    $data = file_get_contents($domain . "/bills?per_page=50&apikey=" . $apikey);
+    $data = file_get_contents($domain . "/bills?per_page=50&apikey=" . $apikey, false, stream_context_create($arrContextOptions));
     header('Content-type: application/json');
     echo $data;
 }
 
 //get bill detials
 if ( $_GET['bill_details'] && $_GET['chamber_type']) {
-    $data = file_get_contents($domain . "/bills?bill_id=" . $_GET['bill_details'] . "&chamber=" . $_GET['chamber_type'] .  "&apikey=" . $apikey);
+    $data = file_get_contents($domain . "/bills?bill_id=" . $_GET['bill_details'] . "&chamber=" . $_GET['chamber_type'] .  "&apikey=" . $apikey, false, stream_context_create($arrContextOptions));
     header('Content-type: application/json');
     echo $data;
 }
 
 //get all house committees
 if ( $_GET['allhousecommittees'] ) {
-    $data = file_get_contents($domain . "/committees?chamber=house&apikey=" . $apikey);
+    $data = file_get_contents($domain . "/committees?chamber=house&apikey=" . $apikey, false, stream_context_create($arrContextOptions));
     header('Content-type: application/json');
     echo $data;
 }
 //get all senate committees
 if ( $_GET['allsenatecommittees'] ) {
-    $data = file_get_contents($domain . "/committees?chamber=senate&apikey=" . $apikey);
+    $data = file_get_contents($domain . "/committees?chamber=senate&apikey=" . $apikey, false, stream_context_create($arrContextOptions));
     header('Content-type: application/json');
     echo $data;
 }
 //get all joint committees
 if ( $_GET['alljointcommittees'] ) {
-    $data = file_get_contents($domain . "/committees?chamber=joint&apikey=" . $apikey);
+    $data = file_get_contents($domain . "/committees?chamber=joint&apikey=" . $apikey, false, stream_context_create($arrContextOptions));
     header('Content-type: application/json');
     echo $data;
 }
